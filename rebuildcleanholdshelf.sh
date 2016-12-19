0d90b6a48d05a18b9737069a12307fd7ca71b82e
Andrew Nisbet <anisbet@epl.ca>
2016-12-19 14:32:49 -0700

    Initial commit of project rebuildcleanholdshelf. All scripts autogenerated by ./create_EPLAPP_shell_script.sh.

---
+++ rebuildcleanholdshelf.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+####################################################
+#
+# Bash shell script for project rebuildcleanholdshelf 
+# Purpose:
+# Method:
+#
+#<one line to give the program's name and a brief idea of what it does.>
+#    Copyright (C) 2015  Andrew Nisbet
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301, USA.
+#
+# Author:  Andrew Nisbet, Edmonton Public Library
+# Copyright (c) 2016
+# Rev: 
+#          0.0 - Dev. 
+#
+####################################################
+
+# Environment setup required by cron to run script because its daemon runs
+# without assuming any environment settings and we need to use sirsi's.
+###############################################
+# *** Edit these to suit your environment *** #
+source /s/sirsi/Unicorn/EPLwork/cronjobscripts/setscriptenvironment.sh
+###############################################
+VERSION=0
+
+# EOF
